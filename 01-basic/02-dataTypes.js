73c5ebeb7a1adafce7931c83a10c6d92b9e7bf1f
dgfhamdaan <122313444+dgfhamdaan@users.noreply.github.com>
2023-08-12 19:17:39 +0000

    datatype

---
+++ 01-basic/02-dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code in newer version
+
+//alert ( 3 + 3 ) // we using node.js not brower
+
+console.log(3
+    +
+    3); // code readibility should be high
+console.log("hamdaan");
+
+let name = 'dgf'
+let age = 18
+let isLogIn = true
+let state;
+
+// number => 2 to power 53 range of number
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
