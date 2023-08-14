97ea7a1903cad00fcafdabc2ce955622ef79a6d8
dgfhamdaan <122313444+dgfhamdaan@users.noreply.github.com>
2023-08-14 20:58:55 +0000

    comparision

---
+++ 01-basic/04-comparision.js
@@ -0,0 +1,30 @@
+console.log(2 > 1);
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+console.log("2" >  1);
+console.log("02" >  1);
+
+console.log(null > 0); 
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+// confussed because null and undefined is treate like null or 0;
+
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
