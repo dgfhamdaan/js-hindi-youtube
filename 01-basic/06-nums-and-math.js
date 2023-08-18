c7f4aa7096ec3b088c86976556aea261fc9537b6
dgfhamdaan <122313444+dgfhamdaan@users.noreply.github.com>
2023-08-18 19:48:33 +0000

    numbers and math

---
+++ 01-basic/06-nums-and-math.js
@@ -0,0 +1,37 @@
+/*
+const score = 300
+console.log(score);
+
+const balance = new Number(200);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.8679;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+*/
+
+/************************Maths****************************/
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //negative value into positive value
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,8,6,7,3));
+// console.log(Math.max(4,8,0,6,7));
+
+console.log(Math.random());
+console.log((Math.random()*10 + 1));
+console.log(Math.floor((Math.random()*10 + 1)));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
