62658186402d6ed551714956b36c29dcaa4ff289
dgfhamdaan <122313444+dgfhamdaan@users.noreply.github.com>
2023-09-20 11:09:56 +0000

    scope level and mini hoisting

--- 03-basic/02-scope.js
+++ 03-basic/02-scope.js
@@ -4,9 +4,55 @@ let a = 300 // outside value called globe scope
 if(true){  // inside value {called block scope}
     let a = 10
     const b = 20 
-    console.log('INNER: ', a); 
+    // console.log('INNER: ', a); 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+function one() {
+    const username = 'hamdaan';
+    
+    function two() {
+        const website = 'youtube';
+        console.log(username);       
+    }
+        // console.log(website);
+    //child variable access to parent variable
+    two();
+
+}
+
+// one();
+
+if(true) {
+    const username = "hamdaan";
+    if(username === "hamdaan") {
+        const website = " youtube";
+        // console.log(username + website);
+    }
+
+    // console.log(website); error is define
+}
+
+// console.log(username); error is define
+
+
+
+/*************************interesting********************/
+
+console.log(addone(5));
+function addone(num) {
+    return num + 1;
+}
+
+// addtwo(5); //hoisting 
+const addtwo = function (num) {
+    return num + 1;
+}
+
+
