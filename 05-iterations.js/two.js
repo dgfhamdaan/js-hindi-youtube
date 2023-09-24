5b67a40f56680a11198717feadaf6b9d7e12577c
dgfhamdaan <122313444+dgfhamdaan@users.noreply.github.com>
2023-09-24 10:20:58 +0000

    while and do while loop

---
+++ 05-iterations.js/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of index: ${index}`);
+    index = index + 2;
+}
+
+let myArray = ['batman','superman','flash'];
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`value of array: ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+// Do while loop
+
+let score = 11;
+do {
+    console.log(`score is ${score}`);
+    score ++;
+} while (score <= 10);
